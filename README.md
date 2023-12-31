502282d2d4535b32a63043455f13c8ec655f5230
Umut Önder <umutsonder@gmail.com>
2023-12-31 12:44:44 +0100

    now needs to be used in subdirectory

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # dotfiles_windows
 My Dotfiles for Windows
+
+.gitconfig needs to be edited to include vscode directory
--- starter.cmd
+++ starter.cmd
@@ -1,2 +1,11 @@
-mklink /d  %LOCALAPPDATA%\nvim %HOMEPATH%\nvim
-mklink /d %LOCALAPPDATA%\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState %HOMEPATH%\windows_terminal\LocalState
+mklink /d  %LOCALAPPDATA%\nvim %HOMEPATH%\dotfiles_windows\nvim
+
+mklink /d %LOCALAPPDATA%\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState %HOMEPATH%\dotfiles_windows\windows_terminal\LocalState
+
+mklink /h %HOMEPATH%\.config\starship.toml %HOMEPATH%\dotfiles_windows\.config\starship.toml
+
+mklink /h %HOMEPATH%\.gitconfig %HOMEPATH%\dotfiles_windows\.gitconfig
+
+mklink /h %HOMEPATH%\.bashrc %HOMEPATH%\dotfiles_windows\.bashrc
+
+mklink /h %HOMEPATH%\.bash_profile %HOMEPATH%\dotfiles_windows\.bash_profile
