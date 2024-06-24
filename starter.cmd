3d0817488fbe78999074ebecaa31faa855f3d19e
Umut Sahin Onder <umutsonder@gmail.com>
2024-06-24 22:57:37 +0200

    Migrated Nvim config to its own repository

--- starter.cmd
+++ starter.cmd
@@ -1,5 +1,3 @@
-mklink /d  %LOCALAPPDATA%\nvim %HOMEPATH%\dotfiles_windows\nvim
-
 mklink /d %LOCALAPPDATA%\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState %HOMEPATH%\dotfiles_windows\windows_terminal\LocalState
 
 mklink /h %HOMEPATH%\.config\starship.toml %HOMEPATH%\dotfiles_windows\.config\starship.toml
